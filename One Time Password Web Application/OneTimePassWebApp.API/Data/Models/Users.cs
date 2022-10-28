614b237b926e30502f9231c0b6c56f7e18e1609a
TNorbi <79305086+TNorbi@users.noreply.github.com>
2022-10-28 14:30:49 +0300

    NuGet packeges added and Users model added

---
+++ One Time Password Web Application/OneTimePassWebApp.API/Data/Models/Users.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace OneTimePassWebApp.API.Data.Models
+{
+    public class Users
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int UserId { get; set; }
+
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    }
+}
