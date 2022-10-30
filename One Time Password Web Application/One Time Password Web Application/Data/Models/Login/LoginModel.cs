faa3548ff25c4dd71e51d1bc5c79fa50436ed717
TNorbi <79305086+TNorbi@users.noreply.github.com>
2022-10-30 19:12:08 +0200

    Login and Register components created

---
+++ One Time Password Web Application/One Time Password Web Application/Data/Models/Login/LoginModel.cs
@@ -0,0 +1,8 @@
+﻿namespace One_Time_Password_Web_Application.Data.Models.Login
+{
+    public class LoginModel
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ One Time Password Web Application/One Time Password Web Application/Data/Models/Register/RegisterModel.cs
@@ -0,0 +1,9 @@
+﻿namespace One_Time_Password_Web_Application.Data.Models.Register
+{
+    public class RegisterModel
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+    }
+}
