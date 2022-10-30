4ffec56c3e08c7098f2de247ffb6dbf9875825c8
TNorbi <79305086+TNorbi@users.noreply.github.com>
2022-10-30 20:09:15 +0200

    Register form validation added

--- One Time Password Web Application/One Time Password Web Application/Data/Models/Register/RegisterModel.cs
+++ One Time Password Web Application/One Time Password Web Application/Data/Models/Register/RegisterModel.cs
@@ -1,9 +1,20 @@
-﻿namespace One_Time_Password_Web_Application.Data.Models.Register
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace One_Time_Password_Web_Application.Data.Models.Register
 {
     public class RegisterModel
     {
+        [Required(ErrorMessage = "Username is required")]
+        [StringLength(20,MinimumLength = 5, ErrorMessage = "Last Name cannot have less than 5 characters and more than 20 characters in length")]
         public string Username { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [StringLength(16, MinimumLength = 5, ErrorMessage = "Password cannot have less than 5 characters and more than 16 characters in length")]
         public string Password { get; set; }
+
+        [Required(ErrorMessage = "Confirm Password is required")]
+        [StringLength(16, MinimumLength = 5, ErrorMessage = "Confirm Password cannot have less than 5 characters and more than 16 characters in length")]
+        [Compare("Password", ErrorMessage = "Password and ConfirmPassword must match")]
         public string ConfirmPassword { get; set; }
     }
 }
