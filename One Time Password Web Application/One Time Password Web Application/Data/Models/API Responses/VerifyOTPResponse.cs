7450423695162ff4dadd9ab1d908617d9acf62a1
TNorbi <79305086+TNorbi@users.noreply.github.com>
2022-10-31 02:38:08 +0200

    ModalDialog commit

---
+++ One Time Password Web Application/One Time Password Web Application/Data/Models/API Responses/VerifyOTPResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace One_Time_Password_Web_Application.Data.Models.API_Responses
+{
+    public class VerifyOTPResponse
+    {
+        public int Code { get; set; }
+        public string Message { get; set; }
+        public long Timestamp { get; set; }
+    }
+}
---
+++ One Time Password Web Application/One Time Password Web Application/Data/Models/Login/VerifyOTPModel.cs
@@ -0,0 +1,11 @@
+﻿namespace One_Time_Password_Web_Application.Data.Models.Login
+{
+    public class VerifyOTPModel
+    {
+        public int UserID { get; set; }
+        public DateTime DateTime { get; set; }
+        public string EnteredOTP { get; set; }
+        public long ExpireDate { get; set; }
+        public string OriginalOTP { get; set; }
+    }
+}
